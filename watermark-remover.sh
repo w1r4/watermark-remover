50f910019dccca381910ff172f27209d3cde5046
w1r4 <wirayudhia@gmail.com>
2022-08-22 17:45:49 +0700

    Update watermark-remover.sh

--- watermark-remover.sh
+++ watermark-remover.sh
@@ -5,7 +5,7 @@ text=$2
 
 pdftk $file output uncompressed.pdf uncompress
 sed -e "/$text/g" < uncompressed.pdf > temp.pdf
-pdftk temp.pdf output fixed.pdf compress
+pdftk temp.pdf output "$text$file" compress
 rm temp.pdf uncompressed.pdf
 
 exit 0
